75da134dfa3e5f43cd6ce9dabe04798454973da0
tanaya0222 <111778605+tanaya0222@users.noreply.github.com>
2023-09-30 00:48:23 +0530

    tanaya changes

--- trial/src/pages/Hotel.jsx
+++ trial/src/pages/Hotel.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 
-const Hotel = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import CommonSection from '../shared/CommonSection';
+import "../style/hotel.css";
+import tours from '../assets/data/tours';
+import NewsLetter from '../shared/NewsLetter';
+import HotelCard from '../shared/HotelCard';
+import SearchBar from '../shared/SearchBar';
+import {Container, Row, Col} from 'reactstrap';
+// import {Row} from 'reactstrap';
 
-export default Hotel
+
+
+const Hotel = () => {
+  return ( 
+  <>
+  <CommonSection title={"All Hotels"}/>
+  <section>
+    <Container>
+      <Row>
+        <SearchBar/>
+      </Row>
+    </Container>
+  </section>
+   
+   <section>
+    <Container>
+      <Row>
+        {
+          tours?.map(hotel=> <Col lg='3' key={hotel.id}> <HotelCard tour={hotel}/> </Col>)
+        } 
+       </Row>
+    </Container>
+   </section>
+
+  </>
+  );
+    
+  
+};
+
+export default Hotel;
